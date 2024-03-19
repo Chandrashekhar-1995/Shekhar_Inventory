b0b284ba6e9fff0067d0905c58d9a3e92d865312
Chandra Shekhar <jmbd.group@gmail.com>
2024-03-19 14:10:26 +0530

    start

---
+++ README.md
@@ -0,0 +1,4 @@
+#  Create project for a inventory management
+    # Start writing Backend
+        - Add MongoosAtlas
+        - Add Git
---
+++ backend/server.js
@@ -0,0 +1,32 @@
+const dotenv = require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const app = express();
+
+//Middlewares
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+app.use(bodyParser.json())
+
+//Routes
+app.get("/", (req, res)=>{
+    res.send("Home Page");
+
+})
+
+
+//Connect to DB and Start server
+
+const PORT = process.env.PORT || 5000;
+
+mongoose
+    .connect(process.env.MONGO_URI)
+    .then(()=>{
+        app.listen(PORT, ()=>{
+            console.log(`server running on port ${PORT}`);
+        })
+    })
+    .catch((err)=>console.log(err));
